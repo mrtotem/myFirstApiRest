0b09ab8a13554c0bd995fb639cbf0c9709930431
mrtotem <octaviod@nimble.la>
2016-12-06 12:27:18 -0300

    FIX: user update and send push

--- FirebaseController.js
+++ FirebaseController.js
@@ -1,32 +1,38 @@
 var FCM = require('fcm-push');
 var config = require('./config');
-
+var user = require('./node_modules/scripts/models/User');
 
 var fcm = new FCM(config.PUSH_SERVER_KEY);
 
 module.exports.sendArrivedPush = function(req, res){
 
-    var arrivedMessage = {
+    user.findById(req.params._userId, function(err, userTemp){
 
-        to: req.body.pushToken, // required fill with device token or topics
-        collapse_key: 'your_collapse_key',
-        notification: {
-            title: 'Mensage de llegada!',
-            body: 'Este es un mensaje de llegada'
-        }
-    };
+            if(err)
+                return res.status(400).send(err);
+    
+            var arrivedMessage = {
 
-    console.log(arrivedMessage);
+                to: userTemp.pushToken, // required fill with device token or topics
+                collapse_key: 'your_collapse_key',
+                notification: {
+                    title: 'Mensage de llegada!',
+                    body: 'Este es un mensaje de llegada'
+                }
+            };
 
-	fcm.send(arrivedMessage, function(err, response){
+            console.log(arrivedMessage);
 
-	    if (err) {
-            console.log(err);
-	        console.log("Something has gone wrong!");
-	    }else{
-            console.log("Successfully sent with response: ", arrivedMessage);
-        }
-	});
+            fcm.send(arrivedMessage, function(err, response){
+
+                if (err) {
+                    console.log(err);
+                    console.log("Something has gone wrong!");
+                }else{
+                    console.log("Successfully sent with response: ", arrivedMessage);
+                }
+            });
+    });
 }
 	
 
